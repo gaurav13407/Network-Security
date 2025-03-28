4f27c9b074cc11efc2abee29b53636b22587b658
gaurav13407 <gaurav9997961651jjk277584@outlook.com>
2025-03-28 07:21:31 +0530

    DAta validation Completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.Data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import sys
-from networksecurity.entity.config_entitiy import DataIngestionConfig
+from networksecurity.entity.config_entitiy import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entitiy import TraningpipelineConfig
 
 if __name__=="__main__":
@@ -13,7 +14,15 @@ if __name__=="__main__":
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initate The data ingesstion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Ingestion is completed")
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(traningpipleconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("initate the data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data validation is completed")
+        print(data_validation_artifact)
+        
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/entity/config_entitiy.py
+++ networksecurity/entity/config_entitiy.py
@@ -32,3 +32,19 @@ class DataIngestionConfig:
         self.train_test_spilt_ratio:float=traning_pipeline.DATA_INGESTION_TRAIN_TEST_SPILT_RATION
         self.collection_name:str=traning_pipeline.DATA_INGESTION_COLEECTION_NAME
         self.database_name:str=traning_pipeline.DATA_INGESTION_DATABASE_NAME
+        
+        
+class DataValidationConfig:
+    def __init__(self,training_pipeline_config:TraningpipelineConfig):
+        self.data_validation_dir:str=os.path.join(training_pipeline_config.artifact_dir,traning_pipeline.DATA_VALIDATION_DIR_NAME)
+        self.valid_data_dir:str=os.path.join(self.data_validation_dir,traning_pipeline.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir:str=os.path.join(self.data_validation_dir,traning_pipeline.DATA_VALIDATION_INVALID_DIR)
+        self.valid_train_file_path:str=os.path.join(self.valid_data_dir,traning_pipeline.TRAIN_FILE_NAME)
+        self.valid_test_file_path:str=os.path.join(self.valid_data_dir,traning_pipeline.TRAIN_FILE_NAME)
+        self.invalid_train_file_path:str=os.path.join(self.invalid_data_dir,traning_pipeline.TRAIN_FILE_NAME)
+        self.invalid_test_file_path:str=os.path.join(self.invalid_data_dir,traning_pipeline.TEST_FILE_NAME)
+        self.drift_report_file_path:str=os.path.join(
+            self.data_validation_dir,
+            traning_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            traning_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+        )
