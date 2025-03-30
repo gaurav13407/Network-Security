0dd9bb44c3fb48fa47228ab48da810099f6a23de
gaurav13407 <gaurav9997961651jjk277584@outlook.com>
2025-03-30 07:59:47 +0530

    DAta Transformation commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.Data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import sys
-from networksecurity.entity.config_entitiy import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entitiy import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entitiy import TraningpipelineConfig
 
 if __name__=="__main__":
@@ -22,6 +23,12 @@ if __name__=="__main__":
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data validation is completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(traningpipleconfig)
+        logging.info("Data Transfromation Started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Datat Transformation Completed")
         
         
     except Exception as e:
